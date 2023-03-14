d8af0079d0bf35e98b847be456896bf9c7de4a81
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-14 15:22:19 +0100

    micro optimizations

--- mapblock.lua
+++ mapblock.lua
@@ -1,9 +1,11 @@
+-- locals
+local char, byte, insert, floor = string.char, string.byte, table.insert, math.floor
 
 local function int_to_bytes(i)
 	local x =i + 32768
-	local h = math.floor(x/256) % 256;
-	local l = math.floor(x % 256);
-	return(string.char(h, l));
+	local h = floor(x/256) % 256
+	local l = floor(x % 256)
+	return(char(h, l))
 end
 
 -- converts a mapblock object to a string
@@ -13,10 +15,10 @@ function mapblock_lib.write_mapblock(mapblock)
 		data = data .. int_to_bytes(mapblock.node_ids[i])
 	end
 	for i=1,4096 do
-		data = data .. string.char(mapblock.param1[i])
+		data = data .. char(mapblock.param1[i])
 	end
 	for i=1,4096 do
-		data = data .. string.char(mapblock.param2[i])
+		data = data .. char(mapblock.param2[i])
 	end
 
 	return data
@@ -47,15 +49,15 @@ function mapblock_lib.read_mapblock(data)
 	for i=1,4096 do
 		-- 1, 3, 5 ... 8191
 		local node_id_offset = (i * 2) - 1
-		local node_id = (string.byte(data, node_id_offset) * 256) +
-		string.byte(data, node_id_offset+1) - 32768
+		local node_id = (byte(data, node_id_offset) * 256) +
+		byte(data, node_id_offset+1) - 32768
 
-		local param1 = string.byte(data, (4096 * 2) + i)
-		local param2 = string.byte(data, (4096 * 3) + i)
+		local param1 = byte(data, (4096 * 2) + i)
+		local param2 = byte(data, (4096 * 3) + i)
 
-		table.insert(mapblock.node_ids, node_id)
-		table.insert(mapblock.param1, param1)
-		table.insert(mapblock.param2, param2)
+		insert(mapblock.node_ids, node_id)
+		insert(mapblock.param1, param1)
+		insert(mapblock.param2, param2)
 	end
 	return mapblock
 end
